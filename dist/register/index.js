b513f153105c9d82027d544cb6ee76f774432e02
yoog <907850084@qq.com>
2022-06-06 11:00:37 +0800

    fix: node-version

--- dist/register/index.js
+++ dist/register/index.js
@@ -11,9 +11,8 @@ const fs_1 = __importDefault(require("fs"));
 const nodeVersion = (process.versions.node.match(/^(\d+)\.(\d+)/) || [])
     .slice(1)
     .map(Number);
-// Use a simple regexp to replace `node:id` with `id` from source code
 function removeNodePrefix(code) {
-    if (nodeVersion[0] <= 14 && nodeVersion[1] < 18) {
+    if (nodeVersion[0] < 14 || (nodeVersion[0] === 14 && nodeVersion[1] < 18)) {
         return code.replace(/([\b\(])require\("node:([^"]+)"\)([\b\)])/g, '$1require("$2")$3');
     }
     return code;
